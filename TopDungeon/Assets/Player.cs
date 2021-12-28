2eb8411aadc5c572c29a15f6440e52d0fea7cac3
Joel Deus <joekiki05@gmail.com>
2021-12-28 23:38:23 +0300

    Moving and Manual Collision Detection

--- TopDungeon/Assets/Player.cs
+++ TopDungeon/Assets/Player.cs
@@ -6,8 +6,9 @@ public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
     private Vector3 moveDelta;
+    private RaycastHit2D hit;
 
-    private void  start()
+    private void  Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
 
@@ -26,8 +27,19 @@ public class Player : MonoBehaviour
         else if (moveDelta.x < 0)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        // make it move
-        transform.Translate(moveDelta * Time.deltaTime);
+        //move in this direction by casting a box first, if the box returns null we're free to move
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider  == null)
+        {
+            // make it move
+            transform.Translate(0,moveDelta.y * Time.deltaTime, 0);
+        }
 
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x,0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            // make it move
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
     }
 }

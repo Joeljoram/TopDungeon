73decd6ea753a725b213cb2e82884017cc368ab0
Joel Deus <joekiki05@gmail.com>
2022-01-11 00:00:01 +0300

    Mover.cs

---
+++ TopDungeon/Assets/Scripts/Mover.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Mover : Fighter
+{
+    protected BoxCollider2D boxCollider;
+    protected Vector3 moveDelta;
+    protected RaycastHit2D hit;
+    protected float yspeed = 0.75f;
+    protected float xspeed = 1.0f;
+
+
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+
+    }
+    protected virtual void UpdateMotor(Vector3 input)
+    {
+
+        //rest MoveDelta
+        moveDelta = new Vector3(input.x * xspeed, input.y * yspeed, 0);
+
+        //swipe sprite direction
+        if (moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        //move in this direction by casting a box first, if the box returns null we're free to move
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            // make it move
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            // make it move
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
+    }
+}
+
+    
+
+
+

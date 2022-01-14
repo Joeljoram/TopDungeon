db6d2568b1b8337c2c183ff1ce6be02e41146aed
Joel Deus <joekiki05@gmail.com>
2022-01-14 21:36:33 +0300

    CharacterMenu.cs

---
+++ TopDungeon/Assets/Scripts/CharacterMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterMenu : MonoBehaviour
+{
+    public Text LevelText, hitpointText, pesosText, upgradecostText, xpText;
+
+    //logic
+    private int currentCharacterSelection;
+    public Image CharacterSelectionSprite;
+    public Image WeaponSprite;
+    public RectTransform xpBar;
+
+
+
+
+
+}
